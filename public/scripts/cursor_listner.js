55cef55f4045062a89c4dcd5830248fb45ab4687
SeePeaJay <seercookie@gmail.com>
2020-02-11 14:21:20 -0800

    finished end scene and button management

--- public/scripts/cursor_listner.js
+++ public/scripts/cursor_listner.js
@@ -1,13 +1,18 @@
 AFRAME.registerComponent('cursor-listener', {
     init: function () {
-      var lastIndex = -1;
-      var COLORS = ['red', 'green', 'blue'];
+      // var lastIndex = -1;
+      // var COLORS = ['red', 'green', 'blue'];
       this.el.addEventListener('click', function (evt) {
-        lastIndex = (lastIndex + 1) % COLORS.length;
-        this.setAttribute('material', 'color', COLORS[lastIndex]);
-        console.log('I was clicked at: ', evt.detail.intersection.point);
-        document.getElementById('Start Scene').setAttribute('visible', 'false');
-        document.getElementById('End Scene').setAttribute('visible', 'true');
+        // lastIndex = (lastIndex + 1) % COLORS.length;
+        // this.setAttribute('material', 'color', COLORS[lastIndex]);
+        // console.log('I was clicked at: ', evt.detail.intersection.point);
+        if (document.getElementById('Start Scene').getAttribute('visible')) {
+          document.getElementById('Start Scene').setAttribute('visible', 'false');
+          document.getElementById('End Scene').setAttribute('visible', 'true');
+        } else if (document.getElementById('End Scene').getAttribute('visible')) {
+          document.getElementById('End Scene').setAttribute('visible', 'false');
+          document.getElementById('Start Scene').setAttribute('visible', 'true');
+        }
       });
     }
   });
