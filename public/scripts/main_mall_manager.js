2420d8d73f1e2158db74f079d144b1dedf06937d
SeePeaJay <seercookie@gmail.com>
2020-04-29 21:08:05 -0700

    can now teleport from game to start area

--- public/scripts/main_mall_manager.js
+++ public/scripts/main_mall_manager.js
@@ -1,34 +1,62 @@
 AFRAME.registerComponent('main-mall-manager', {
     schema: {
-        isPlayerReady: {type: 'boolean', default: false}
+        isPlayerReady: {type: 'boolean', default: false},
+        isInStartArea: { type: 'boolean', default: true},
+        isInGameArea: {type: 'boolean', default: false},
+        isInFailArea: {type: 'boolean', default: false},
+        isInEndArea: {type: 'boolean', default: false},
     },
     init: function () {
-        document.getElementById('Start Scene').setAttribute('visible', 'true');
-        document.getElementById('timer_text').setAttribute('visible', 'false');
+        // document.getElementById('Start Scene').setAttribute('visible', 'true');
+        // document.getElementById('timer_text').setAttribute('visible', 'false');
 
-        document.getElementById('Game Scene').setAttribute('visible', 'false');
-        document.getElementById('minus_text').setAttribute('visible', 'false');
-        document.getElementById('plus_text').setAttribute('visible', 'false');
+        // document.getElementById('Game Scene').setAttribute('visible', 'false');
+        // document.getElementById('minus_text').setAttribute('visible', 'false');
+        // document.getElementById('plus_text').setAttribute('visible', 'false');
 
-        document.getElementById('Fail Scene').setAttribute('visible', 'false');
-        document.getElementById('fail_scene_button').removeAttribute('class');
+        // // document.getElementById('Fail Scene').setAttribute('visible', 'false');
+        // document.getElementById('fail_scene_button').removeAttribute('class');
 
-        document.getElementById('End Scene').setAttribute('visible', 'false');
-        document.getElementById('fail_scene_button').removeAttribute('class');
+        // // document.getElementById('End Scene').setAttribute('visible', 'false');
+        // document.getElementById('fail_scene_button').removeAttribute('class');
 
-        var data = this.data;
-        var scene = document.querySelector('a-scene');
-        scene.addEventListener('mousedown', () => {
-            console.log("I am pressed");
-            if (document.getElementById('Game Scene').getAttribute('visible')) {
-                data.isPlayerReady = true;
-            }
-        });
+        // var data = this.data;
+        // var scene = document.querySelector('a-scene');
+        // scene.addEventListener('mousedown', () => {
+        //     console.log("I am pressed");
+        //     if (document.getElementById('Game Scene').getAttribute('visible')) {
+        //         data.isPlayerReady = true;
+        //     }
+        // });
         // THREE.DefaultLoadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
         //     console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
         // };
     },
-    switchToStartScene: function () {
+    teleportToGameArea: function () {
+        // disable start button
+        document.getElementById('start_scene_button').removeAttribute('class');
+        document.getElementById('cursor').setAttribute('visible', 'false');
+        // teleport
+        document.getElementById('rig').setAttribute('position', '0 0 0');
+        // toggle isInStartArea
+        this.data.isInStartArea = false;
+        // toggle isInGameArea 
+        this.data.isInGameArea = true;
+        // add press trigger when you are ready text
+        document.getElementById('timer_text').setAttribute("text", "value: Press trigger when you're ready");
+    },
+    teleportToFailArea: function () {
+        document.getElementById("Game Scene").setAttribute("visible", "false");
+        document.getElementById("timer_text").setAttribute("material", "opacity: 0");
+
+        document.getElementById("Fail Scene").setAttribute("visible", "true");
+        document.getElementById("fail_scene_button").setAttribute("class", "clickable");
+        document.getElementById("cursor").setAttribute("visible", "true");
+    },
+    teleportToEndArea: function () {
+
+    },
+    teleportToStartArea: function () {
         if (document.getElementById('Fail Scene').getAttribute('visible')) {
             document.getElementById('Fail Scene').setAttribute('visible', 'false');
             document.getElementById('fail_scene_button').removeAttribute('class');
@@ -40,24 +68,5 @@ AFRAME.registerComponent('main-mall-manager', {
 
         document.getElementById('Start Scene').setAttribute('visible', 'true');
         document.getElementById('start_scene_button').setAttribute('class', 'clickable');
-    },
-    switchToGameScene: function () {
-        document.getElementById('Start Scene').setAttribute('visible', 'false');
-        document.getElementById('start_scene_button').removeAttribute('class');
-        document.getElementById('cursor').setAttribute('visible', 'false');
-
-        document.getElementById('Game Scene').setAttribute('visible', 'true');
-        document.getElementById('timer_text').setAttribute('visible', 'true');
-    },
-    switchToFailScene: function () {
-        document.getElementById("Game Scene").setAttribute("visible", "false");
-        document.getElementById("timer_text").setAttribute("material", "opacity: 0");
-
-        document.getElementById("Fail Scene").setAttribute("visible", "true");
-        document.getElementById("fail_scene_button").setAttribute("class", "clickable");
-        document.getElementById("cursor").setAttribute("visible", "true");
-    },
-    switchToEndScene: function () {
-
     }
 });
--- public/scripts/start_scene_cursor_listener.js
+++ public/scripts/start_scene_cursor_listener.js
@@ -1,9 +1,8 @@
 AFRAME.registerComponent('start-scene-cursor-listener', {
   init: function () {
     this.eventHandler = () => {
-      console.log("Start scene status: " + document.getElementById('Start Scene').getAttribute('visible'));
-      if (document.getElementById('Start Scene').getAttribute('visible')) {
-        document.getElementById('Main Mall Manager').components['main-mall-manager'].switchToGameScene();
+      if (document.getElementById('main_mall_manager').getAttribute('main-mall-manager').isInStartArea) {
+        document.getElementById('main_mall_manager').components['main-mall-manager'].teleportToGameArea();
       }
     };
     this.el.addEventListener('click', this.eventHandler); // handler name only, not function with ()
