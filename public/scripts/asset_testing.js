4a96dbceb610aa37eda4aaf592f649c743db3a41
Dante Cerron <dacerron@gmail.com>
2021-01-29 13:50:42 -0800

    fixed missing assets

---
+++ public/scripts/asset_testing.js
@@ -0,0 +1,25 @@
+const startTime = Date.now()
+THREE.DefaultLoadingManager.onProgress = function(url, itemsLoaded, itemsTotal) {
+    const now = Date.now()
+    var dt = now - startTime;
+    console.log("loading file" + url + " " + itemsLoaded + " of " + itemsTotal + 'time(ms): ' + dt);
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    
+    console.log('starting timing')
+    const assets = document.querySelectorAll("a-asset-item")
+    assets.forEach((element) => {
+        element.addEventListener('loaded', () => {
+            const name = element.getAttribute("id")
+            const now = Date.now();
+            var dt = now - startTime;
+            console.log(name + ":" + dt);
+        })
+    })
+    if(console.log(assets));
+})
+
+
+    // console log asset loading tim
