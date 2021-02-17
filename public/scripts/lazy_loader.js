f9445d7771ecf402dc1c4c897062a27af6c86ff5
Zahra Asadi <zahraasadi@dhcp-206-12-71-111.ubcsecure.wireless.ubc.ca>
2021-02-17 13:45:35 -0800

    loading

--- public/scripts/lazy_loader.js
+++ public/scripts/lazy_loader.js
@@ -1,14 +1,19 @@
 AFRAME.registerComponent("lazy-loader", {
     init: function () {
+  
         const lazyLoader = {}
         lazyLoader.triggerLoad = function (el) {
             const id = el.getAttribute("id");
             console.log('[LAZY LOADER]loading element with id ' + id);
             if (id.includes("boba")) {
                 //load boba model by adding obj-model component
+                el.setAttribute("obj-model", "obj: boba; mtl: boba-mtl");
             } else if (id.includes("coffee")) {
                 //load coffee model by adding obj-model component 
-            }// add if else cases for the other types of model
+                el.setAttribute("obj-model", "obj: coffee; mtl: coffee-mtl");
+            } else if (id.includes("timbit")) {          
+                el.setAttribute("obj-model", "obj: donut; mtl: donut-mtl");     
+            }
         }
         const collectibles = document.querySelectorAll(".collectible");
         collectibles.forEach((element) => {
