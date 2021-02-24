6921611e83ddda3733ea2fc3f881787507e29520
Zahra Asadi <zahraasadi@dhcp-206-12-66-220.ubcsecure.wireless.ubc.ca>
2021-02-24 12:43:03 -0800

    new changes

--- public/scripts/student-pool.js
+++ public/scripts/student-pool.js
@@ -1,15 +1,16 @@
 AFRAME.registerComponent("student-pool", {
     init(){
-        this.studentPool = document.querySelector("a-scene").components.pool
-        console.log(document.querySelector("a-scene").components);
+        console.log("INITIALIZING STUDENT COMPONENTS")
+        this.studentPool = this.el.sceneEl.components.pool
+        console.log(this.el.sceneEl.components);
         this.playerCamera = document.getElementById("camera");
     },
     tick(){
-        if(this.studentPool != undefined) {
+        if(this.studentPool !== undefined) {
             console.log("STUDENT POOL INSTANTIATING");
             let student = this.studentPool.requestEntity()
             let cameraPosition = this.playerCamera.getAttribute("position");
-            student.setAttribute("animation", "from:" + cameraPosition.x - 10 + " " + cameraPosition.y + " " + cameraPosition.z - 10 + "; to:"+ cameraPosition.x + 10 + " " + cameraPosition.y + " " + cameraPosition.z - 10)
+            student.setAttribute("animation", "from:" + cameraPosition.x - 10 + " " + cameraPosition.y + " " + cameraPosition.z - 10 + "; to:"+ cameraPosition.x + 10 + " " + cameraPosition.y + " " + cameraPosition.z + 10)
             student.play();
         } else {
         }
