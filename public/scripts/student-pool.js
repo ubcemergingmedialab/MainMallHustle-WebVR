038f7bd15e9c18998c6a2cb81d133d06e6577a6f
Dante Cerron <dacerron@gmail.com>
2021-02-26 13:56:41 -0800

    person loads and animatiosn

--- public/scripts/student-pool.js
+++ public/scripts/student-pool.js
@@ -13,9 +13,14 @@ AFRAME.registerComponent("student-pool", {
             }
             console.log("INSTANTIATED STUDENT");
             let cameraPosition = this.playerCamera.getAttribute("position");
-            student.setAttribute("animation", "from:" + cameraPosition.x - 10 + " " + cameraPosition.y + " " + cameraPosition.z - 10 + "; to:"+ cameraPosition.x + 10 + " " + cameraPosition.y + " " + cameraPosition.z + 10)
             student.play();
-            console.log(student.getAttribute("position"))
+            const animationString = "property:position; from: " + (cameraPosition.x - 10) + " 2 " + (cameraPosition.z - 10) + "; to:" + cameraPosition.x + 10 + " 2 " + cameraPosition.z + 10 + ";dur: 10000"
+            // TODO: generate a random integer from 1 to 9
+            // build id for person image based on random int. EG) "#person" + (randomInt) -> "#person3" 
+            // use generated to pass into student material. EG) student.setAttribute("material", "src: " + [generatedId]) -> student.setAttribute("material", "src:#person3")
+            student.setAttribute("animation", animationString)
+            student.setAttribute("material", "src: #person3");
+            console.log(student.getAttribute("animation"))
         } else {
         }
     },
