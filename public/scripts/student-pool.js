ea013fcedf41946341acaa41dec9909ed87ff7c1
Dante Cerron <dacerron@gmail.com>
2021-03-24 11:36:03 -0700

    more stuff

--- public/scripts/student-pool.js
+++ public/scripts/student-pool.js
@@ -16,7 +16,7 @@ AFRAME.registerComponent("student-pool", {
             let cameraPosition = this.playerCamera.getAttribute("position");
             student.play();
        
-            let randomX = (Math.round(Math.random()) * 2 - 1) * (Math.floor(Math.random() * 5) + 2);
+            let randomX = (Math.round(Math.random()) * 2 - 1) * (Math.floor(Math.random() * 5));
             console.log(randomX)
             let startX = randomX
             let endX = -randomX
@@ -25,7 +25,7 @@ AFRAME.registerComponent("student-pool", {
 
             console.log("ANIMATION: "+endZ)
 
-            let animationString = "property:position; from: " + startX + " 2 " + startZ + "; to:" + endX + " 2 " + endZ + ";dur: 2000; easing:linear"
+            let animationString = "property:position; from: " + startX + " 2 " + startZ + "; to:" + endX + " 2 " + endZ + ";dur: 1000; easing:linear"
             let randomInt = Math.floor(Math.random() * 9) + 1;    //generate a random integer from 1 to 9
             //console.log(this.randomInt)
             this.generatedId = "#person" + (randomInt);       // build id for person image based on random int
@@ -37,7 +37,7 @@ AFRAME.registerComponent("student-pool", {
             queue.push(student)
             //console.log(student.setAttribute("material", "src: " + this.generatedId));
             console.log(student.getAttribute("animation"))
-        }, 2000);
+        }, 1000);
     },
     tick() {
     
